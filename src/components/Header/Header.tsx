be4ec37060282c449f42fcb012d0b72f78724406
Renas Barış Özkal <barisozkal11@gmail.com>
2022-08-08 22:05:37 +0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+function Header(){
+    return (
+        <div className="header">
+            <a href='#'>Hello</a>
+            <a href='#'>asdasd</a>
+            <a href='#'>qweqwe</a>
+        </div>
+    )
+}
+export default Header;
