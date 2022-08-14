ef59eef11fe8f1ff5e994fa0d7accbcfb29853ef
Renas Barış Özkal <barisozkal11@gmail.com>
2022-08-14 19:25:48 +0300

    sidebar is implemented :: other body components will be implemented

--- src/Features/Body/Component/Body.tsx
+++ src/Features/Body/Component/Body.tsx
@@ -1,13 +1,49 @@
-import React from "react";
-import { BodyComponent, BodySC } from "./BodySC";
+import React, { useState } from "react";
+import { BodyComponent, BodySC, SideBar } from "./BodySC";
 import welcome from "../../../assets/welcome.png";
 import personalSummary from "../../../assets/personalSummary.png";
-
+import "./SideBar.css"
 
 const Body = () => {
+  let openSideBar = false;
+  const [isOpen, setIsopen] = useState(false);
+
+  const ToggleSidebar = () => {
+    isOpen === true ? setIsopen(false) : setIsopen(true);
+  };
   return (
     <BodySC>
-      <i id="list-icon" className="fa-solid fa-bars"></i>
+      <i
+        onClick={ToggleSidebar}
+        id="list-icon"
+        className="fa-solid fa-bars"
+      ></i>
+      <div className={`sidebar ${isOpen == true ? "active" : ""}`}>
+        <div className="sd-header">
+          <h4 className="mb-0">Sidebar Header</h4>    
+        </div>
+        <div className="sd-body">
+          <ul>
+            <li>
+              <a className="sd-link">Welcome to My Page</a>
+            </li>
+            <li>
+              <a className="sd-link">About Me</a>
+            </li>
+            <li>
+              <a className="sd-link">My Projects</a>
+            </li>
+            <li>
+              <a className="sd-link">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div
+        className={`sidebar-overlay ${isOpen == true ? "active" : ""}`}
+        onClick={ToggleSidebar}
+      ></div>
+
       <BodyComponent
         className="welcome"
         style={{ backgroundImage: `url(${personalSummary})` }}
@@ -15,8 +51,9 @@ const Body = () => {
         <div className="welcome-card">
           <h1>WELCOME TO MY PAGE</h1>
           <p>
-            This is Renas Barış Özkal. In this page you can find my personal summary, projects that I have made and contributed,
-            my education history and my experience.
+            This is Renas Barış Özkal. In this page you can find my personal
+            summary, projects that I have made and contributed, my education
+            history and my experience.
           </p>
         </div>
       </BodyComponent>
--- src/Features/Body/Component/BodySC.tsx
+++ src/Features/Body/Component/BodySC.tsx
@@ -1,69 +1,78 @@
 import styled from "styled-components";
 
 export const BodySC = styled.div`
-    & i{
-        position: fixed;
-        top: 0;
-        left: 0;
-        margin-top: 5%;
-        margin-left: 5%;
-        width: 20px;
-        height: 20px;
-        border: 1px solid #fff;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        padding: 20px;
-        background-color: #ffffff;
-        border-radius: 50%;
-        opacity: 0.5;
-        transition: opacity 0.3s ease-in-out;
-        
-        @media (min-width: 745px) {
+  & i {
+    position: fixed;
+    top: 0;
+    left: 0;
+    margin-top: 5%;
+    margin-left: 5%;
+    width: 20px;
+    height: 20px;
+    border: 1px solid #fff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+    background-color: #ffffff;
+    border-radius: 50%;
+    opacity: 0.5;
+    transition: opacity 0.3s ease-in-out;
 
-            display: none;
-        }
-        &:hover{
-            cursor: pointer;
-            opacity: 1;
-        }
-        &:active{
-          
-        }
+    @media (min-width: 746px) {
+      display: none;
     }
-    
-
+    &:hover {
+      cursor: pointer;
+      opacity: 1;
+    }
+  }
 `;
 
 export const BodyComponent = styled.div`
-    display: flex;
-   
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center center;
-    background-color: #464646;
-    height:100vh;
-    overflow: hidden;
+  display: flex;
 
-    & .welcome-card{
-      //border: 1px solid black;
-      height: 65%;
-      width: 50%;
-      margin-left: 10%;
-      & h1{
-        font-size: 46px;
-        color: #073bac;
-        font-family: 'Catamaran', sans-serif;
-      }
-      & p{
-        font-size: 20px;
-        font-family: 'Comfortaa', cursive;
-        line-height:200%;
-      }
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  background-color: #464646;
+  height: 100vh;
+  overflow: hidden;
+
+  & .welcome-card {
+    //border: 1px solid black;
+    height: 65%;
+    width: 50%;
+    margin-left: 10%;
+    & h1 {
+      font-size: 46px;
+      color: #073bac;
+      font-family: "Catamaran", sans-serif;
     }
+    & p {
+      font-size: 20px;
+      font-family: "Comfortaa", cursive;
+      line-height: 200%;
+    }
+  }
 `;
+export const SideBar = styled.div`
+  @media (min-width: 745px) {
+      display: none;
+  }
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: lightblue;
+  color: red;
+  display: block;
+  flex-direction: column;
+`;
+
 export default BodySC;
