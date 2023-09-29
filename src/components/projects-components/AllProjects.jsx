3b621087613663fee325c7c67aa7d7375e1db44d
rbarisozkal <barisozkal11@gmail.com>
2023-09-29 10:57:29 +0300

    projects page completed

--- src/components/projects-components/AllProjects.jsx
+++ src/components/projects-components/AllProjects.jsx
@@ -8,11 +8,69 @@ import css from "../../assets/css.svg";
 import swift from "../../assets/swift.svg";
 import java from "../../assets/java.svg";
 import python from "../../assets/python.svg";
+import boschSVG from "../../assets/boschSVG.svg";
+import huaweiSvg from "../../assets/huaweiSVG.svg";
 import React from "react";
 import axios from "axios";
+const companyProjects = [
+  {
+    name: "AMR Management",
+    id: 1,
+    description:
+      "AMR Management is a web application developed using Angular, Rxjs, ChartJs, OpenStreetMap and Angular Material. It is a management dashboard for AMR's in each branch of Bosch. It contains comparison of AMR's, their locations, their status and their history and performance by time.",
+    language: "TypeScript",
+    logo: boschSVG.src,
+    project: "company",
+    company: "Bosch",
+  },
+  {
+    name: "Operator Dashboard",
+    id: 2,
+    description:
+      "Operator Dashboard is a web application developed using Angular, Rxjs and Angular Material. It is a dashboard for analyzing the operator's status at the department BMG (Bosch Manufacturing Solutions).",
+    language: "TypeScript",
+    logo: boschSVG.src,
+    project: "company",
+    company: "Bosch",
+  },
+  {
+    name: "Test Equipment Reservation",
+    id: 3,
+    description:
+      "Test Equipment Reservation is a web application developed using Angular, Rxjs, Angular Material and .NetCore. It is a dashboard for reserving test equipment for the department BMG (Bosch Manufacturing Solutions).",
+    language: "TypeScript",
+    logo: boschSVG.src,
+    project: "company",
+    company: "Bosch",
+  },
+  {
+    name: "Movie Ticketing System",
+    id: 4,
+    description:
+      "Movie Ticketing System is a web application developed using Vue, JavaScript, TypeScript and Vuetify. It is an application for reserving movie tickets for the cinema company located at Myanmar",
+    language: "Vue",
+    logo: huaweiSvg.src,
+    project: "company",
+    company: "Huawei",
+  },
+  {
+    name: "Huawei Cloud Lottery",
+    id: 5,
+    description:
+      "This is an app for advertising Huawei Cloud products. It promotes Huawei Cloud products by giving away prizes to the users. It is developed using Vue, JavaScript, TypeScript and Vuetify.",
+    language: "Vue",
+    logo: huaweiSvg.src,
+    project: "company",
+    company: "Huawei",
+  },
+];
 
 function AllProjects() {
   const [projects, setProjects] = useState([]);
+  const companyProject = {
+    Bosch: boschSVG.src,
+    Huawei: huaweiSvg.src,
+  };
   const [loading, setLoading] = useState(true);
   const getLanguageIcon = {
     TypeScript: typescript.src,
@@ -36,6 +94,8 @@ function AllProjects() {
         response.data.sort(function (a, b) {
           return new Date(b.created_at) - new Date(a.created_at);
         });
+        //push company projects to the top of the list
+        response.data.unshift(...companyProjects);
         setProjects(response.data);
         console.log(response.data);
       });
@@ -47,17 +107,26 @@ function AllProjects() {
           className="bg-white rounded-lg shadow-lg overflow-hidden m-4 max-w-4xl w-full"
           key={repo.id}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            className="h-12 w-12 m-2 fill-current text-gray-600"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.164 6.839 9.49.5.09.682-.218.682-.486 0-.24-.009-.875-.013-1.713-2.782.603-3.369-1.338-3.369-1.338-.454-1.154-1.11-1.462-1.11-1.462-.905-.619.068-.607.068-.607 1.003.07 1.532 1.03 1.532 1.03.89 1.527 2.337 1.086 2.904.831.09-.645.35-1.086.638-1.336-2.23-.255-4.576-1.116-4.576-4.962 0-1.097.393-1.995 1.04-2.696-.105-.255-.452-1.278.099-2.665 0 0 .846-.271 2.769 1.03A9.67 9.67 0 0112 5.85c.825.005 1.657.111 2.447.328 1.923-1.3 2.768-1.03 2.768-1.03.552 1.387.206 2.41.101 2.665.647.701 1.038 1.599 1.038 2.696 0 3.858-2.35 4.704-4.592 4.954.36.312.682.924.682 1.862 0 1.345-.012 2.425-.012 2.75 0 .27.18.584.688.485C19.138 20.16 22 16.415 22 12c0-5.523-4.477-10-10-10z"
+          {" "}
+          {repo.project === "company" ? (
+            <img
+              className="w-48 p-4"
+              src={companyProject[repo.company]}
+              alt=""
             />
-          </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className="h-12 w-12 m-2 fill-current text-gray-600"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.164 6.839 9.49.5.09.682-.218.682-.486 0-.24-.009-.875-.013-1.713-2.782.603-3.369-1.338-3.369-1.338-.454-1.154-1.11-1.462-1.11-1.462-.905-.619.068-.607.068-.607 1.003.07 1.532 1.03 1.532 1.03.89 1.527 2.337 1.086 2.904.831.09-.645.35-1.086.638-1.336-2.23-.255-4.576-1.116-4.576-4.962 0-1.097.393-1.995 1.04-2.696-.105-.255-.452-1.278.099-2.665 0 0 .846-.271 2.769 1.03A9.67 9.67 0 0112 5.85c.825.005 1.657.111 2.447.328 1.923-1.3 2.768-1.03 2.768-1.03.552 1.387.206 2.41.101 2.665.647.701 1.038 1.599 1.038 2.696 0 3.858-2.35 4.704-4.592 4.954.36.312.682.924.682 1.862 0 1.345-.012 2.425-.012 2.75 0 .27.18.584.688.485C19.138 20.16 22 16.415 22 12c0-5.523-4.477-10-10-10z"
+              />
+            </svg>
+          )}
           <div className="p-4">
             <h2 className="font-semibold text-lg">{repo.name}</h2>
             <p className="text-gray-600">{repo.full_name}</p>
